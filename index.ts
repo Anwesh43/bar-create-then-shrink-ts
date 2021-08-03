d938fcfe1cbd3ef698778544452ca61559a74c79
Anwesh43 <anweshthecool0@gmail.com>
2021-08-03 15:05:48 +0530

    adding BCTSNode class

--- index.ts
+++ index.ts
@@ -118,3 +118,45 @@ class Animator {
         }
     }
 }
+
+class BCTSNode {
+
+    prev : BCTSNode 
+    next : BCTSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BCTSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBCTSNode(context, this.i, this.state.scale)
+    }
+    
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BCTSNode {
+        var curr : BCTSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
